725bd7d60a0f726f5579a5b12508faa0c707854a
keshav <keshavkoirala933@gmail.com>
2024-02-28 18:32:33 +0545

    Initial commit

---
+++ todo-list.js
@@ -0,0 +1,51 @@
+const todoList = [{
+   name:'Bank account of keshav koirala',
+   dueDate: '2025-08-21'
+ }, {
+   name: 'github status of keshav koirala',
+dueDate:'2025-08-21'
+}];
+
+renderTodoList();
+function renderTodoList() {
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+   const todoObject = todoList[i];
+   //const name = todoObject.name;
+   //const dueDate = todoObject.dueDate;
+   const{name, dueDate } = todoObject;
+   const html = `
+   <div>${name}</div>
+   <div>${dueDate}</div>
+   <button onclick="
+   todoList.splice(${i}, 1);
+   renderTodoList();
+   " class="delete" >Delete</button>
+   `;
+   todoListHTML += html;
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+   .innerHTML = todoListHTML;
+}
+
+function addTodo() {
+   const inputElement = document.querySelector('.js-name-input');
+   const name = inputElement.value;
+   const dateInputElement = document.querySelector('.js-due-date-input');
+   const dueDate = dateInputElement.value;
+
+   todoList.push({
+     // name: name,
+     // dueDate: dueDate,
+      name,
+      dueDate
+   });
+   console.log(todoList);
+
+   inputElement.value = '';
+
+   renderTodoList();
+}
